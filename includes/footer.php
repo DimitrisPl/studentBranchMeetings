82daad496bd05c9157a79b53352ffed274467a6b
Alexander Dimitriadis <alex_necromancer@yahoo.gr>
2017-04-30 20:35:59 +0300

    Temp Store
    Temp checkpoint

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<!-- footer -->
+<div class="row">
+	<div class="col-md-12">
+		<div class="navbar-default-bottom navbar-default footer">
+			<br/><br/>
+			<p> All rights reserved. </p>
+		</div>
+	</div>
+</div>
